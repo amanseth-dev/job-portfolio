18f6a59f71245fe1b5719ebc0fb95200b2923f0c
Aman Seth <amanthecoder04@gmail.com>
2025-08-11 11:33:39 +0000

    The navbar links: make it align in the center of the page for the websit

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,28 +13,29 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center">
-        <div className="mr-4 flex">
+      <div className="container flex h-14 max-w-screen-2xl items-center justify-between">
+        <div className="flex items-center">
           <Link href="/" className="mr-6 flex items-center space-x-2">
             <Code className="h-6 w-6 text-primary" />
             <span className="font-bold sm:inline-block">
               Aman Kr. Seth
             </span>
           </Link>
-          <nav className="hidden gap-6 md:flex">
-            {navLinks.map((link) => (
-              <Link
-                key={link.name}
-                href={link.href}
-                className="flex items-center text-lg font-medium text-foreground/60 transition-colors hover:text-foreground/80 sm:text-sm"
-              >
-                {link.name}
-              </Link>
-            ))}
-          </nav>
         </div>
 
-        <div className="flex flex-1 items-center justify-end space-x-2">
+        <nav className="hidden gap-6 md:flex">
+          {navLinks.map((link) => (
+            <Link
+              key={link.name}
+              href={link.href}
+              className="flex items-center text-lg font-medium text-foreground/60 transition-colors hover:text-foreground/80 sm:text-sm"
+            >
+              {link.name}
+            </Link>
+          ))}
+        </nav>
+
+        <div className="flex items-center justify-end space-x-2">
           <ThemeToggle />
           <Sheet open={isOpen} onOpenChange={setIsOpen}>
             <SheetTrigger asChild>

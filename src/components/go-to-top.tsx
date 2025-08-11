3495e08b098a69770d0425edfa09abcb5820b50a
Aman Seth <amanthecoder04@gmail.com>
2025-08-11 12:46:24 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button size="icon

--- src/components/go-to-top.tsx
+++ src/components/go-to-top.tsx
@@ -38,9 +38,10 @@ export default function GoToTop() {
         size="icon"
         onClick={scrollToTop}
         className={cn(
-          'rounded-full transition-opacity duration-300',
+          'rounded-full transition-opacity duration-300 bg-background/60 backdrop-blur-sm hover:bg-background/80',
           isVisible ? 'opacity-100' : 'opacity-0 pointer-events-none'
         )}
+        variant="outline"
         aria-label="Go to top"
       >
         <ArrowUp className="h-6 w-6" />

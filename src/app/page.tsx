0db1ba3a619a7092e498777ed7e6aa17fd34a49b
Aman Seth <amanthecoder04@gmail.com>
2025-08-11 10:54:41 +0000

    Add 10px of padding more

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Footer from '@/components/footer';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-background px-[10px]">
+    <div className="flex flex-col min-h-screen bg-background px-[20px]">
       <Header />
       <main className="flex-grow">
         <Hero />

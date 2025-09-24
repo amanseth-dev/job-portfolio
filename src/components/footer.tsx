e6d233defebc521738ee94aef998f86c7b0290dc
Aman Seth <amanthecoder04@gmail.com>
2025-09-24 20:46:22 +0000

    I have "Get In Touch" section which have links to Github and linkedIn, s

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,22 +1,13 @@
-import { Github, Linkedin } from 'lucide-react';
 import Link from 'next/link';
 
 export default function Footer() {
   return (
     <footer className="border-t">
-      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
+      <div className="container flex flex-col items-center justify-center gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
             © {new Date().getFullYear()} Aman Kumar Seth. All Rights Reserved.
           </p>
-        </div>
-        <div className="flex items-center gap-4 md:mr-8">
-          <Link href="https://github.com/aman1784" target="_blank" rel="noopener noreferrer">
-            <Github className="h-6 w-6 text-muted-foreground transition-colors hover:text-foreground" />
-          </Link>
-          <Link href="https://www.linkedin.com/in/aman-seth-926833195" target="_blank" rel="noopener noreferrer">
-            <Linkedin className="h-6 w-6 text-muted-foreground transition-colors hover:text-foreground" />
-          </Link>
         </div>
       </div>
     </footer>
